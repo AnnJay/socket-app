ca17f68ee7b014326a6cc7a0457d7d6efec550ce
Ann Jay <orange_world@inbox.ru>
2025-03-04 16:29:18 +0300

    chore: add db connection

---
+++ server/src/lib/database.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const connectDataBase = () =>
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .then((value) => {
+      console.log("MONGODB host is: ", value.connection.host);
+    })
+    .catch((error) => console.log(error));
+
+module.exports = connectDataBase;
--- server/src/main.js
+++ server/src/main.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
 const { authRoutes } = require("./routes/auth.route");
+const connectDataBase = require("./lib/database");
 
 const app = express();
-const PORT = 5000;
+dotenv.config();
+
+const PORT = process.env.PORT;
 
 app.use("/api/auth", authRoutes);
 
-app.listen(PORT, () => console.log(`Сервер запущен на порте ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на порте ${PORT}`);
+  connectDataBase();
+});
