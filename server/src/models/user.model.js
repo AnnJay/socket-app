d93bf315632d1594ec4ba839951b7a27f1c8592c
Ann Jay <orange_world@inbox.ru>
2025-03-04 18:07:43 +0300

    feat: add User model

---
+++ server/src/models/user.model.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 6,
+    },
+    avatar: {
+      type: String,
+      dafault: "",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
