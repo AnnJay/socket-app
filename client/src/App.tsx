4a3c5f9c56398230fd1d86230a3c368a1229c711
Ann Jay <orange_world@inbox.ru>
2025-03-06 13:59:13 +0300

    chore: client side preparings

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,26 @@
 import React from "react"
+import { Navigation } from "./components/Navigation"
+import { Route, Routes } from "react-router-dom"
+import { HomePage } from "./pages/HomePage"
+import { SignUpPage } from "./pages/SignUpPage"
+import { LoginPage } from "./pages/LoginPage"
+import { SettingsPage } from "./pages/SettingsPage"
+import { ProfilePage } from "./pages/ProfilePage"
+import { NotFound } from "./pages/NotFound"
 
-const App = () => {
-  return <div>socket-app</div>
+export const App = () => {
+  return (
+    <>
+      <Navigation />
+
+      <Routes>
+        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </>
+  )
 }
-
-export default App
