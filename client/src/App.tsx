601cb45069744c0e6318e52d93ca21eef61e9199
Ann Jay <orange_world@inbox.ru>
2025-03-06 17:37:15 +0300

    feat: add routes protection, invoke  checkAuth function within app update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,44 @@
-import React from "react"
+import { Navigate, Route, Routes } from "react-router-dom"
+import { useEffect } from "react"
+import { Loader } from "lucide-react"
+
 import { Navigation } from "./components/Navigation"
-import { Route, Routes } from "react-router-dom"
 import { HomePage } from "./pages/HomePage"
 import { SignUpPage } from "./pages/SignUpPage"
 import { LoginPage } from "./pages/LoginPage"
 import { SettingsPage } from "./pages/SettingsPage"
 import { ProfilePage } from "./pages/ProfilePage"
 import { NotFound } from "./pages/NotFound"
+import { useAuthStore } from "./store/useAuthStore"
 
 export const App = () => {
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore()
+
+  useEffect(() => {
+    checkAuth()
+
+    return () => {}
+  }, [])
+
+  if (isCheckingAuth && !authUser) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    )
+  }
+
   return (
     <>
       <Navigation />
 
       <Routes>
         <Route path="*" element={<NotFound />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="/sign-up" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path="/sign-up" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
         <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
       </Routes>
     </>
   )
