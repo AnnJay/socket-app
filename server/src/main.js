e2f3dbf7455906659c3b23f2aeaeaedb3cf0f256
Ann Jay <orange_world@inbox.ru>
2025-03-06 15:16:12 +0300

    chore: add cors

--- server/src/main.js
+++ server/src/main.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const dotenv = require("dotenv")
 const cookieParser = require("cookie-parser")
+const cors = require("cors")
 
 const { authRoutes } = require("./routes/auth.route")
 const { messageRoutes } = require("./routes/message.route")
@@ -11,6 +12,12 @@ dotenv.config()
 
 const PORT = process.env.PORT
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+)
 app.use(express.json())
 app.use(cookieParser())
 
