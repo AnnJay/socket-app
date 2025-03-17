51012dbf660335bc7794f5ea557ce5fd0501eeb3
Ann Jay <orange_world@inbox.ru>
2025-03-17 11:03:43 +0300

    feat: add ChatPreview component

---
+++ client/src/components/ChatPreview.tsx
@@ -0,0 +1,75 @@
+import { SendHorizontal } from "lucide-react"
+import { FC } from "react"
+
+const MOCK_CHAT = [
+  {
+    id: 1,
+    text: "Значит, наши с ним стихии идеально совпадают?",
+    isSent: false,
+    avatar: "/default_avatar.webp",
+    name: "Naruto Uzumaki",
+  },
+  {
+    id: 2,
+    text: "Да, только ветер может победить молнию",
+    isSent: true,
+    avatar: "https://res.cloudinary.com/dcyl8cn9p/image/upload/v1742196902/Kakashi700_mb6rry.webp",
+    name: "Kakashi Hatake",
+  },
+  {
+    id: 3,
+    text: "Нет! Только ветер может сделать огонь еще сильнее!",
+    isSent: false,
+    avatar: "/default_avatar.webp",
+    name: "Naruto Uzumaki",
+  },
+]
+
+export const ChatPreview: FC = () => {
+  return (
+    <div className="mt-6">
+      <h2 className="text-2xl font-bold text-base-content">Превью чата</h2>
+
+      <div className="bg-base-300 rounded-md flex justify-center p-4">
+        <div className="bg-base-100 max-w-lg w-full p-4 rounded-md space-y-7">
+          <div className="flex gap-4 justify-start items-center">
+            <img src="/default_avatar.webp" className="rounded-full border-2 size-16" />
+            <div>
+              <p className="font-medium text-sm">Naruto Uzumaki</p>
+              <p className="text-xs text-base-content/70">Online</p>
+            </div>
+          </div>
+          <div className="space-y-4">
+            {MOCK_CHAT.map((message) => (
+              <div key={message.id} className={`chat chat-${message.isSent ? "end" : "start"}`}>
+                <div className="chat-image avatar">
+                  <div className="w-10 rounded-full border-2">
+                    <img alt="avatar" src={message.avatar} />
+                  </div>
+                </div>
+                <div className="chat-header">
+                  {message.name}
+                  <time className="text-xs opacity-50 mx-2">12:45</time>
+                </div>
+                <div className="chat-bubble">{message.text}</div>
+              </div>
+            ))}
+
+            <div className="flex items-center justify-between gap-2 w-full">
+              <input
+                type="text"
+                placeholder="Превью чата"
+                readOnly
+                className="input input-bordered  rounded-md w-full"
+              />
+
+              <div className="flex items-center justify-center border-2 rounded-md size-12 bg-base-300 hover:bg-base-100 cursor-pointer">
+                <SendHorizontal className="size-8 text-secondary" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- client/src/pages/SettingsPage.tsx
+++ client/src/pages/SettingsPage.tsx
@@ -1,3 +1,4 @@
+import { ChatPreview } from "../components/ChatPreview"
 import { ThemeButton } from "../components/ThemeButton"
 import { THEMES } from "../constants"
 import { useTheme } from "../store/useTheme"
@@ -8,14 +9,16 @@ export const SettingsPage = () => {
   return (
     <div className="h-screen container mx-auto pt-8">
       <div className="flex flex-col space-y-6">
-        <h2 className="text-2xl">Темы</h2>
+        <h2 className="text-2xl font-bold">Темы</h2>
         <p className="text-lg text-base-content ">Подберите тему по вкусу</p>
 
-        <div className="grid grid-cols-6 gap-4 ">
+        <div className="grid grid-cols-4 md:grid-cols-6 gap-4 ">
           {THEMES.map((themeItem) => (
-            <ThemeButton theme={themeItem} onClick={() => setTheme(themeItem)} />
+            <ThemeButton key={themeItem} theme={themeItem} onClick={() => setTheme(themeItem)} />
           ))}
         </div>
+
+        <ChatPreview />
       </div>
     </div>
   )
