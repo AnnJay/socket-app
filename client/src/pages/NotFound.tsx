3b6b9b5d412a4cd928147175897439ade8978262
Ann Jay <orange_world@inbox.ru>
2025-03-21 15:11:03 +0300

    refactor: change favicon, add styles to not found page

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import { Link } from "react-router-dom"
 
 export const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className="h-screen w-full flex flex-col justify-center items-center space-y-6">
+      <h1 className="text-2xl">Упс, страничка пропала</h1>
+      <p>Вернитесь на главную</p>
+      <button className="btn btn-accent">
+        <Link to="/">Главная</Link>
+      </button>
+    </div>
   )
 }
