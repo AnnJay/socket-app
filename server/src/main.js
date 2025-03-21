5823f34195f0cae285ee140a7e883474e57bb8b8
Ann Jay <orange_world@inbox.ru>
2025-03-21 15:43:09 +0300

    chore: deploy preparations

--- server/src/main.js
+++ server/src/main.js
@@ -31,10 +31,10 @@ app.use("/api/auth", authRoutes)
 app.use("/api/message", messageRoutes)
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(path.resolve(), "../frontend/dist")))
+  app.use(express.static(path.join(path.resolve(), "../client/dist")))
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(path.resolve(), "../frontend", "dist", "index.html"))
+    res.sendFile(path.join(path.resolve(), "../client", "dist", "index.html"))
   })
 }
 
